ab025cb203489de715cf72d5233c39e21e53a098
ridwanfbnr <ridwanfebnur9@gmail.com>
2022-08-02 14:04:57 +0700

    route parameter

---
+++ test/route-parameter.test.js
@@ -0,0 +1,43 @@
+import express from "express";
+import request from "supertest";
+
+const app = express();
+
+app.get("/products/:id", (req, res) => {
+    const idProduct = req.params.id;
+    res.send(`idProduct: ${idProduct}`);
+});
+
+app.get("/categories/:id(\\d+)", (req, res) => {
+    const idCategory = req.params.id;
+    res.send(`idCategory: ${idCategory}`);
+});
+
+app.get("/products/:id(\\d+)/categories/:name", (req, res) => {
+    // const { idProduct, nameCategory } = req.params;
+    const idProduct = req.params.id;
+    const nameCategory = req.params.name;
+    res.send(`idProduct: ${idProduct}, nameCategory: ${nameCategory}`);
+});
+
+test("Test Route Parameter", async () => {
+    let response = await request(app).get("/products/shirt");
+    expect(response.text).toBe("idProduct: shirt");
+
+    response = await request(app).get("/products/pants");
+    expect(response.text).toBe("idProduct: pants");
+
+    response = await request(app).get("/categories/011");
+    expect(response.text).toBe("idCategory: 011");
+
+    response = await request(app).get("/categories/salah.json");
+    expect(response.status).toBe(404);
+});
+
+test("Test Route Parameter 2", async () => {
+    let response = await request(app).get("/products/001/categories/shirt");
+    expect(response.text).toBe("idProduct: 001, nameCategory: shirt");
+
+    response = await request(app).get("/products/01-salah/categories/shirt");
+    expect(response.status).toBe(404);
+});
